e8d058af89b8575895af6377640b75a7e03c2de8
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-03-13 13:53:09 -0700

    Move data (from controller) to service factory

--- app/assets/javascripts/main/mainCtrl.js
+++ app/assets/javascripts/main/mainCtrl.js
@@ -1,8 +1,38 @@
 angular
 .module('myAngularApp')
+.factory('postsFlow', [
+  function(){
+    var object = {
+      posts: [
+        {
+          title: 'The Other Works of George Martin',
+          votes: 10
+        },
+        {
+          title: 'Food Origins',
+          votes: 11
+        },
+        {
+          title: 'It\'s a College Basketball Vacation',
+          votes: 2
+        },
+        {
+          title: 'Sports and Measures',
+          votes: 10
+        },
+        {
+          title: 'A Short History of Brokered Conventions',
+          votes: 8
+        }
+      ]
+    }
+    return object;
+  }
+])
 .controller('MainCtrl', [
   '$scope',
-  function($scope){
+  'postsFlow',
+  function($scope, postsFlow){
 
     $scope.addPost = function(){
       $scope.posts.push({title: $scope.title, votes: 0});
@@ -12,27 +42,6 @@ angular
       post.votes += 1;
     };
 
-    $scope.posts = [
-      {
-        title: 'The Other Works of George Martin',
-        votes: 10
-      },
-      {
-        title: 'Food Origins',
-        votes: 11
-      },
-      {
-        title: 'It\'s a College Basketball Vacation',
-        votes: 2
-      },
-      {
-        title: 'Sports and Measures',
-        votes: 10
-      },
-      {
-        title: 'A Short History of Brokered Conventions',
-        votes: 8
-      }
-    ]
+    $scope.posts = postsFlow.posts;
   }
 ]);
